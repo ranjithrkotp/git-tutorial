abb3158246ccb96798f822ad6e8f8f9ab57be705
RANJITH R K <31716@fisat>
2018-04-18 01:11:26 -0400

    implemented adding 3 numbers

--- add.py
+++ add.py
@@ -1,4 +1,11 @@
 a=int(raw_input("Enter the 1st number"))
 b=int(raw_input("Enter 2nd number"))
 c=a+b
-print "sum is ",c
+print "sum of entered 2 numbers is",c
+def add3():
+    num1=int(raw_input("Enter 1st number"))
+    num2=int(raw_input("Enter 2nd number"))
+    num3=int(raw_input("Enter 3rd number"))
+    s=num1+num2+num3
+    print "sum is of three numbers entered is",s
+add3()
